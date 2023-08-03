e2c2e0964bb3cdf6decd5fc02646552ce4b26974
Max Goryunov <maximthehedgehog@yandex.ru>
2023-08-03 23:11:40 +0300

    #1 equality

--- LinearSystemsToolkit/Vector.c
+++ LinearSystemsToolkit/Vector.c
@@ -58,3 +58,19 @@ double scalarMultiplication(Vector* row, Vector* col) {
 	}
 	return sum;
 }
+
+char vectorEqual(Vector* left, Vector* right) {
+	char equal = 1;
+	if (left->n != right->n) {
+		equal = 0;
+	}
+	else {
+		for (int i = 0; i < left->n; ++i) {
+			if (left->data[i] != right->data[i]) {
+				equal = 0;
+				break;
+			}
+		}
+	}
+	return equal;
+}
