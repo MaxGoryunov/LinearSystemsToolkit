8f55f5583e6a94a816797f6d8c9f449733dfc4e5
Max Goryunov <maximthehedgehog@yandex.ru>
2023-08-15 00:22:22 +0300

    #12 diff test

--- LinearSystemsToolkit/VectorTest.c
+++ LinearSystemsToolkit/VectorTest.c
@@ -5,29 +5,55 @@
 * vectorSum can produce a sum of two vectors.
 */
 void sumsTwoVectors() {
-	int size = 3;
-	Vector* left = vectorCreate(size);
+	int size      = 3;
+	Vector* left  = vectorCreate(size);
 	Vector* right = vectorCreate(size);
-	Vector* sum = vectorCreate(size);
+	Vector* sum   = vectorCreate(size);
 	for (int i = 0; i < size; ++i) {
-		left->data[i] = i;
+		left->data[i]  = i;
 		right->data[i] = i * 2;
-		sum->data[i] = i * 3;
+		sum->data[i]   = i * 3;
 	}
 	assert(1 == vectorEqual(vectorSum(left, right), sum));
 }
+/**
+ * @todo #12:30m/DEV There is no operation for freeing the memory when the
+ *  vector is no longer needed. There must be a function which takes a 
+ *  vector and frees memory reserved for it. This behaviour in tests must
+ *  be checked by special methods which detect memory leakages.
+*/
+
+/**
+ * vectorDiff returns a difference of two vectors.
+*/
+void calculatesDiffOfTwoVectors() {
+	int size      = 3;
+	Vector* left  = vectorCreate(size);
+	Vector* right = vectorCreate(size);
+	Vector* diff  = vectorCreate(size);
+	for (int i = 0; i < size; ++i) {
+		left->data[i]  = i + 4;
+		right->data[i] = i + 1;
+		diff->data[i]  = 3;
+	}
+	assert(1 == vectorEqual(vectorDiff(left, right), diff));
+}
+
 
 void vectorAllTests() {
 	sumsTwoVectors();
 }
 
 /**
-* @todo #1:45m/DEV The issue does not specify the required code coverage for
-*  this feature, this is why I decided that it is enough to just create
-*  functions listed in the issue. It is important to add some tests to check
-*  that the remaining functions also work as intended.
+ * @todo #12:45min/DEV Tests for Common.c were not written in this PR because
+ *  the issue only mentions Vector tests. However, functionality in Common.c
+ *  also needs to be covered by tests to ensure correctness.
 */
-
+/**
+ * @todo #12:30m/DEV There will probably be other test files, not only Vector
+ *  tests. Vector tests have to be reorganized so that all tests can be run
+ *  using one command, meaning that all tests are built as a single app.
+*/
 int main() {
 	vectorAllTests();
 }
