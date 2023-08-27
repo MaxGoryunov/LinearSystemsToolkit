2e443436b993aaa75bf6798b4644c5ee7e8bd59a
Max Goryunov <maximthehedgehog@yandex.ru>
2023-08-27 23:29:20 +0300

    #18 reorganise test functions

--- LinearSystemsToolkit/CommonTest.c
+++ LinearSystemsToolkit/CommonTest.c
@@ -2,7 +2,14 @@
 #include "CommonTest.h"
 #include "Common.h"
 
+/**
+ * safeMalloc can return allocated memory of correct size.
+*/
 void safeMallocReturnsAllocatedMemoryOfCorrectSize() {
     size_t size = 16;
     assert(size == sizeof(SafeMalloc(size)));
+}
+
+void commonAllTests() {
+    safeMallocReturnsAllocatedMemoryOfCorrectSize();
 }
--- LinearSystemsToolkit/CommonTest.h
+++ LinearSystemsToolkit/CommonTest.h
@@ -1,6 +1,3 @@
 #pragma once
 
-/**
- * safeMalloc can return allocated memory of correct size.
-*/
-void safeMallocReturnsAllocatedMemoryOfCorrectSize();
+void commonAllTests();
