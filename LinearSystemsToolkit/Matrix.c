f4bd1a06054a4b205247cde5ecfc34de23325a3f
Max Goryunov <maximthehedgehog@yandex.ru>
2023-08-31 23:49:45 +0300

    #24 print, sum, diff, scale

--- LinearSystemsToolkit/Matrix.c
+++ LinearSystemsToolkit/Matrix.c
@@ -1,4 +1,5 @@
 #include <malloc.h>
+#include <stdio.h>
 #include "Matrix.h"
 
 Matrix* matrixCreate(int n) {
@@ -32,3 +33,53 @@ Matrix* matrixIdentity(int n) {
     return M;
 }
 
+Matrix* matrixReadFromFile(const char* name, int n) {
+    FILE* f = fopen(name, "r");
+    Matrix* A = matrixCreate(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            fscanf(f, "%lf", &(A->data[i][j]));
+        }
+    }
+    fclose(f);
+    return A;
+}
+
+void matrixPrint(Matrix* M) {
+    for (int i = 0; i < M->n; ++i) {
+        for (int j = 0; j < M->n; ++j) {
+            printf("%lf ", M->data[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+Matrix* matrixSum(Matrix* left, Matrix* right) {
+	Matrix* S = matrixCreate(left->n);
+	for (int i = 0; i < left->n; ++i) {
+		for (int j = 0; j < right->n; ++j) {
+			S->data[i][j] = left->data[i][j] + right->data[i][j];
+		}
+	}
+	return S;
+}
+
+Matrix* matrixDiff(Matrix* left, Matrix* right) {
+	Matrix* D = matrixCreate(left->n);
+	for (int i = 0; i < left->n; ++i) {
+		for (int j = 0; j < right->n; ++j) {
+			D->data[i][j] = left->data[i][j] - right->data[i][j];
+		}
+	}
+	return D;
+}
+
+Matrix* matrixScale(Matrix* A, double num) {
+	Matrix* S = matrixCreate(A->n);
+	for (int i = 0; i < A->n; ++i) {
+		for (int j = 0; j < A->n; ++j) {
+			S->data[i][j] = A->data[i][j] * num;
+		}
+	}
+	return S;
+}
