11f7c87d75073bfb7864aa398bd4d50345a5de64
Max Goryunov <maximthehedgehog@yandex.ru>
2023-09-01 14:34:37 +0300

    #18 check architecture

--- LinearSystemsToolkit/CommonTest.c
+++ LinearSystemsToolkit/CommonTest.c
@@ -1,17 +1,20 @@
+#include <limits.h>
 #include <assert.h>
 #include <malloc.h>
 #include "CommonTest.h"
 #include "Common.h"
 
+int arch() {
+    return CHAR_BIT * sizeof(void*);
+}
+
 /**
  * safeMalloc can return allocated memory of correct size.
 */
 void safeMallocReturnsAllocatedMemoryOfCorrectSize() {
-    size_t size = 16;
-    void* ptr = SafeMalloc(size);
-    printf("sizeof(ptr) = %i\n", sizeof(ptr));
-    printf("sizeof(int) = %i\n", sizeof(int));
-    assert(sizeof(int) == sizeof(int));
+    void* ptr = SafeMalloc(16);
+    int expected = (arch() == 64) ? sizeof(double) : sizeof(int);
+    assert(expected == sizeof(ptr));
     free(ptr);
 }
 
