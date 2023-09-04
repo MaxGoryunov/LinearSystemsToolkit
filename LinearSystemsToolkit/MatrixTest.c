1e9e35378c2b2bd7deb25d91f009c80153eee7c6
Max Goryunov <maximthehedgehog@yandex.ru>
2023-09-04 08:37:17 +0300

    #24 test diff, scale

--- LinearSystemsToolkit/MatrixTest.c
+++ LinearSystemsToolkit/MatrixTest.c
@@ -35,6 +35,46 @@ void matrixSumSumsTwoMatrices() {
     matrixDestroy(S);
 }
 
+/**
+ * matrixDiff can compute the difference of two matrices.
+*/
+void matrixDiffComputesDifferenceOfTwoMatrices() {
+    int size  = 3;
+    Matrix* A = matrixCreate(size);
+    Matrix* B = matrixCreate(size);
+    Matrix* D = matrixCreate(size);
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            A->data[i][j] = i + j;
+            B->data[i][j] = j;
+            D->data[i][j] = i;
+        }
+    }
+    assert(1 == matrixEqual(matrixDiff(A, B), D));
+    matrixDestroy(A);
+    matrixDestroy(B);
+    matrixDestroy(D);
+}
+
+/**
+ * matrixScale can scale a matrix by some factor.
+*/
+void matrixScaleScalesMatrixBySomeFactor() {
+    int size  = 3;
+    double factor = 3;
+    Matrix* A = matrixCreate(size);
+    Matrix* S = matrixCreate(size);
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            A->data[i][j] = i + j;
+            S->data[i][j] = (i + j) * factor;
+        }
+    }
+    assert(1 == matrixEqual(matrixScale(A, factor), S));
+    matrixDestroy(A);
+    matrixDestroy(S);
+}
+
 void matrixAllTests() {
     matrixIdentityReturnsIdentityMatrix();
     matrixSumSumsTwoMatrices();
